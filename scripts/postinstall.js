7dc8188e96d0fc291e0faa7c02e42d4cf3d9d683
Jesus David García Gomez <dgarcia@plainconcepts.com>
2017-10-02 16:10:06 -0700

    Add Visual C++ Redistributable and postinstall script
    * Add Visual C++ Redistributable and postinstall script
    * Update protocol.json
    * Update protocol.json
    
    * Fix travis error

---
+++ scripts/postinstall.js
@@ -0,0 +1,11 @@
+const child = require('child_process');
+const path = require('path');
+
+const arch = process.arch;
+
+console.log(`Installing Microsoft Visual C++ 2017 Redistributable (${arch})`);
+const command = `${path.join(__dirname, '..', 'redistributable', `VC_redist.${arch}.exe`)} /quiet /norestart`;
+
+child.execSync(command);
+
+console.log(`Microsoft Visual C++ 2017 Redistributable (${arch}) Installed`);
