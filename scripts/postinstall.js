bfb622718d7959e7286f773ab7fa27a45a7d12d4
Anton Molleda <antonmo@microsoft.com>
2017-10-06 11:56:24 -0700

    Fix: Check if VC++ is installed

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,11 +1,31 @@
 const child = require('child_process');
 const path = require('path');
-
+const regedit = require('regedit');
 const arch = process.arch === 'x64' ? 'x64' : 'x86';
+const keys = {
+    x64: 'HKCR\\Installer\\Dependencies\\,,amd64,14.0,bundle',
+    x86: 'HKCR\\Installer\\Dependencies\\,,x86,14.0,bundle'
+};
+const key = keys[arch];
 
-console.log(`Installing Microsoft Visual C++ 2017 Redistributable (${arch})`);
-const command = `${path.join(__dirname, '..', 'redistributable', `VC_redist.${arch}.exe`)} /quiet /norestart`;
+regedit.list(key, function (err, result) {
+    if(result){
+        console.log('VC++ 2017 seems to be installed. Skipping installation.');
+        console.log('If you have any problems, please install it manually from the "redistributable" folder.');
 
-child.execSync(command);
+        return;
+    }
 
-console.log(`Microsoft Visual C++ 2017 Redistributable (${arch}) Installed`);
+    if(err){
+        console.error('Error trying to determine if VC++ 2017 Redistributable is installed. Installing it anyways...');
+    }
+
+    console.log(`Installing Microsoft Visual C++ 2017 Redistributable (${arch})`);
+    const command = `${path.join(__dirname, '..', 'redistributable', `VC_redist.${arch}.exe`)} /quiet /norestart`;
+    try {
+        child.execSync(command);
+        console.log(`VC++ 2017 Redistributable (${arch}) Installed`);
+    } catch (e) {
+        console.error('Error installing VC++ 2017 Redistributable, please install it manually from the "redistributable" folder');
+    }
+});
